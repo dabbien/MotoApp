1427e814d06070d6cab39a273b755fe7a71c9efa
dabbien <dabbienoski@gmail.com>
2023-03-26 20:15:07 +0200

    Day 1

---
+++ MotoApp/BasicStack.cs
@@ -0,0 +1,13 @@
+﻿namespace MotoApp
+{
+    internal class BasicStack<T>
+    {
+        private readonly T[] _items;
+        public BasicStack() => _items = new T[10];
+        private int _currentIndex = -1;
+
+        public void Push(T item) => _items[++_currentIndex] = item;
+        public T Pop() => _items[_currentIndex--];
+        public int Count => _currentIndex + 1;
+    }
+}
--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MotoApp;
+
+var stack = new BasicStack<double>();
+stack.Push(4.3);
+stack.Push(433);
+stack.Push(3.2);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+Console.WriteLine($"Total: {sum}");
